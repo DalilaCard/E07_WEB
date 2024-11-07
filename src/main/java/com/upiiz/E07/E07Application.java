2bcdd9a5213db7f6a80e54ffb98cabb979fee385
Dalila Card <sernacardonadalila@gmail.com>
2024-11-06 20:58:58 -0600

    primer commit

---
+++ src/main/java/com/upiiz/E07/E07Application.java
@@ -0,0 +1,13 @@
+package com.upiiz.E07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E07Application.class, args);
+	}
+
+}
